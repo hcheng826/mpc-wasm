c9250471dfdabf0a2fe848fe6b117107184d3107
hcheng826 <lilioo826@gmail.com>
2022-12-23 10:37:18 +0800

    compiled but the surf client cannot be reached in join_computation

--- src/gg20_signing.rs
+++ src/gg20_signing.rs
@@ -1,5 +1,10 @@
-use anyhow::{anyhow, Context, Result};
 use serde_json::Value;
+use std::convert::TryInto;
+use anyhow::{anyhow, Context, Result};
+use futures::{Sink, Stream, StreamExt, TryStreamExt};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use round_based::Msg;
+use round_based::async_runtime::AsyncProtocol;
 
 macro_rules! log {
     ( $( $t:tt )* ) => {
@@ -18,13 +23,14 @@ pub async fn sign(
     let local_share: Value = serde_json::from_str(&local_share).context("parse local share")?;
     let number_of_parties = parties.len();
 
-    log!("{:?}", local_share);
+    // log!("{:?}", local_share);
+
+    // "::<String>" is a guess below
+    let (i, incoming, outgoing) = join_computation::<String>(address.clone(), &format!("{}-offline", room))
+        .await
+        .context("join offline computation")?;
 
     Ok("test".to_string())
-
-    // let (i, incoming, outgoing) = join_computation(address.clone(), &format!("{}-offline", room))
-    //     .await
-    //     .context("join offline computation")?;
 
     // let incoming = incoming.fuse();
 
@@ -76,4 +82,123 @@ pub async fn sign(
     // let signature = serde_json::to_string(&signature).context("serialize signature")?;
 
     // Ok(signature)
+}
+
+
+pub async fn join_computation<M>(
+    address: surf::Url,
+    room_id: &str,
+) -> Result<(
+    u16,
+    impl Stream<Item = Result<Msg<M>>>,
+    impl Sink<Msg<M>, Error = anyhow::Error>,
+)>
+where
+    M: Serialize + DeserializeOwned,
+{
+    log!("join_computation: {:?}", room_id);
+    log!("join_computation: {:?}", address);
+
+    let client = SmClient::new(address, room_id).context("construct SmClient")?;
+
+    log!("client");
+
+    // Construct channel of incoming messages
+    let incoming = client
+        .subscribe()
+        .await
+        .context("subscribe")?
+        .and_then(|msg| async move {
+            serde_json::from_str::<Msg<M>>(&msg).context("deserialize message")
+        });
+
+    // Obtain party index
+    let index = client.issue_index().await.context("issue an index")?;
+
+    // Ignore incoming messages addressed to someone else
+    let incoming = incoming.try_filter(move |msg| {
+        futures::future::ready(
+            msg.sender != index && (msg.receiver.is_none() || msg.receiver == Some(index)),
+        )
+    });
+
+    // Construct channel of outgoing messages
+    let outgoing = futures::sink::unfold(client, |client, message: Msg<M>| async move {
+        let serialized = serde_json::to_string(&message).context("serialize message")?;
+        client
+            .broadcast(&serialized)
+            .await
+            .context("broadcast message")?;
+        Ok::<_, anyhow::Error>(client)
+    });
+
+    Ok((index, incoming, outgoing))
+}
+
+pub struct SmClient {
+    http_client: surf::Client,
+}
+
+impl SmClient {
+    pub fn new(address: surf::Url, room_id: &str) -> Result<Self> {
+
+        log!("SmClient: {:?}", room_id);
+        log!("SmClient: {:?}", address);
+
+        let config = surf::Config::new()
+            .set_base_url(address.join(&format!("rooms/{}/", room_id))?)
+            .set_timeout(None);
+
+        log!("SmClient: {:?}", config);
+
+        Ok(Self {
+            http_client: config.try_into()?,
+        })
+    }
+
+    pub async fn issue_index(&self) -> Result<u16> {
+        let response = self
+            .http_client
+            .post("issue_unique_idx")
+            .recv_json::<IssuedUniqueIdx>()
+            .await
+            .map_err(|e| e.into_inner())?;
+        Ok(response.unique_idx)
+    }
+
+    pub async fn broadcast(&self, message: &str) -> Result<()> {
+        self.http_client
+            .post("broadcast")
+            .body(message)
+            .await
+            .map_err(|e| e.into_inner())?;
+        Ok(())
+    }
+
+    pub async fn subscribe(&self) -> Result<impl Stream<Item = Result<String>>> {
+        let response = self
+            .http_client
+            .get("subscribe")
+            .await
+            .map_err(|e| e.into_inner())?;
+        let events = async_sse::decode(response);
+        Ok(events.filter_map(|msg| async {
+            match msg {
+                Ok(async_sse::Event::Message(msg)) => Some(
+                    String::from_utf8(msg.into_bytes())
+                        .context("SSE message is not valid UTF-8 string"),
+                ),
+                Ok(_) => {
+                    // ignore other types of events
+                    None
+                }
+                Err(e) => Some(Err(e.into_inner())),
+            }
+        }))
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct IssuedUniqueIdx {
+    unique_idx: u16,
 }
--- www/index.js
+++ www/index.js
@@ -8,18 +8,21 @@ const smButton = document.getElementById("sm-button");
 const SERVICE_URL = "http://localhost:8002";
 const SM_MANAGER_URL = "http://localhost:8000";
 
+let room_id = uuidv4();
+console.log("room_id: ", room_id);
+
 smButton.addEventListener("click", async (event) => {
     const localShare = document.getElementById("local-share").value;
     const message = document.getElementById("message").value;
-    const room_id = uuidv4();
 
-    console.log(await wasm.sm_sign(message, localShare, SM_MANAGER_URL, room_id));
+    console.log("result in index.js", await wasm.sm_sign(message, localShare, SM_MANAGER_URL, room_id));
 })
 
 theOtherShareButton.addEventListener("click", (event) => {
-    axios.get(SERVICE_URL).then(res => console.log(res));
-    // axios.post(`${SERVICE_URL}/sign`, {
-    //     msg: message,
-    //     room_id: room_id
-    // });
+    // axios.get(SERVICE_URL).then(res => console.log(res));
+    const message = document.getElementById("message").value;
+    axios.post(`${SERVICE_URL}/sign`, {
+        msg: message,
+        room_id: room_id
+    });
 })
